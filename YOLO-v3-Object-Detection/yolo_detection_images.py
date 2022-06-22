477fed633567ca3bea764a05a939e89beeb3309c
황지원 <41350477+HWNAGJIWON@users.noreply.github.com>
2022-06-22 12:35:13 +0900

    Update yolo_detection_images.py

--- YOLO-v3-Object-Detection/yolo_detection_images.py
+++ YOLO-v3-Object-Detection/yolo_detection_images.py
@@ -1,15 +1,31 @@
 import numpy as np
+import os
 import cv2
-
+from email.mime import image
+from flask import Flask, jsonify, request
+#from yolo_detection_images import detectObjects
+# from flask_cors import CORS
+from werkzeug.utils import secure_filename
+# import os
+# from flask_restx import Api, Resource
 
 def detectObjects(img_path):
     confidenceThreshold = 0.5
     NMSThreshold = 0.3
 
-    modelConfiguration = 'cfg/yolov3.cfg'
-    modelWeights = 'yolov3.weights'
+    #modelConfiguration = 'cfg/yolov3.cfg'
+    #modelWeights = 'yolov3.weights'
 
-    labelsPath = 'obj.names'
+    #labelsPath = 'obj.names'
+    #CUR_DIR = os.path.abspath('.')
+    modelConfiguration = '/home/g2019sun0925/flask_yolo_api/YOLO-v3-Object-Detection/cfg/yolov3.cfg'
+    #modelConfiguration = os.path.join(CUR_DIR,'cfg/yolov3.cfg')
+    modelWeights = '/home/g2019sun0925/flask_yolo_api/YOLO-v3-Object-Detection/yolov3.weights'
+
+    #modelWeights = os.path.join(CUR_DIR,'yolov3.weights')
+    labelsPath = '/home/g2019sun0925/flask_yolo_api/YOLO-v3-Object-Detection/obj.names'
+    #labelsPath = os.path.join(CUR_DIR, 'obj.names')
+    
     labels = open(labelsPath).read().strip().split('\n')
 
     np.random.seed(10)
@@ -18,8 +34,9 @@ def detectObjects(img_path):
     net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
 
     image = cv2.imread(img_path)
+    #image = image.resize(int(image.width/2),int(image.height/2))
     (H, W) = image.shape[:2]
-
+    print("h:w = ", H, W)
     #Determine output layer names
     layerName = net.getLayerNames()
     layerName = [layerName[i - 1] for i in net.getUnconnectedOutLayers()] # layerName = [layerName[i[0] - 1] for i in net.getUnconnectedOutLayers()]
@@ -53,19 +70,25 @@ def detectObjects(img_path):
     outputs={}
 
     if len(detectionNMS)>0:
-        outputs['detections']={}
-        outputs['detections']['labels']=[]
+        #outputs['detections']={}
+        #outputs['detections']['labels']=[]
         
         for i in detectionNMS.flatten():
-            detection={}
-            detection['Label']=labels[classIDs[i]]
-            detection['confidence']=confidences[i]
-            detection['X']=boxes[i][0]
-            detection['Y']=boxes[i][1]
-            detection['Width']=boxes[i][2] # width
-            detection['Height']=boxes[i][3] # height
-            outputs['detections']['labels'].append(detection)
+            #detection={}
+            #detection['Label']=labels[classIDs[i]]
+            outputs['detections']=1
+            outputs['label']=labels[classIDs[i]]
+
+            #detection['confidence']=confidences[i]
+            outputs['confidence']=confidences[i]
+            #detection['X']=boxes[i][0]
+            #detection['Y']=boxes[i][1]
+            #detection['Width']=boxes[i][2] # width
+            #detection['Height']=boxes[i][3] # height
+            #outputs['detections']['labels'].append(detection)
     else:
-        output['detections']='No object detected'
-        
+        outputs['detections']=0
+        outputs['label']=' '
+        outputs['confidence']= 0
+
     return outputs
